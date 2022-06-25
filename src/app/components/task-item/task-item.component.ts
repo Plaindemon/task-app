57fccc9249c7980575c8dd8a46fd56516a933bac
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-06-25 09:56:50 -0600

    delete task function working

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Task } from '../../Tasks'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -7,9 +9,16 @@ import { Task } from '../../Tasks'
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task;
+  faTimes = faTimes;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task) {
+    console.log(task)
+    this.onDeleteTask.emit(task)
   }
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock.tasks';
 import {Task} from '../../Tasks';
+import {TaskService} from '../../services/task.service'
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -8,11 +8,15 @@ import {Task} from '../../Tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
-  
-  constructor() { }
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t=> t.id !== task.id))
+  }
 }
